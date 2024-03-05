708113f77fcb7f54346e3540f4e5d3b5f9c957ae
Amelia Gustafson <amelia.gustafson200@gmail.com>
2024-03-05 11:33:02 -0600

    working generateGid function, add borders to see it

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const container = document.getElementById('grid-container');
+
 // make grid
 function generateGrid() {
-     
+     for (var i=0; i < 256; i++) {
+        var gridItem = document.createElement("div");
+gridItem.setAttribute('id', 'gridItem');
+       container.appendChild(gridItem);
+     }
 }            
