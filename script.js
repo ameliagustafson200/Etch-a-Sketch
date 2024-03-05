37599b7eeec060bc2bb4187bd698a2629d9ae8c4
Amelia Gustafson <amelia.gustafson200@gmail.com>
2024-03-05 11:43:45 -0600

    make it look more like a grid

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.getElementById('grid-container');
 
 // make grid
 function generateGrid() {
-     for (var i=0; i < 256; i++) {
+     for (var i=0; i < 257; i++) {
         var gridItem = document.createElement("div");
 gridItem.setAttribute('id', 'gridItem');
        container.appendChild(gridItem);
