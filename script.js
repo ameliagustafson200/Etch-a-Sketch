321a2ec23f30891450836bd9db7141b673c85664
Amelia Gustafson <amelia.gustafson200@gmail.com>
2024-03-07 13:29:03 -0600

    I dont think I ended up keeping anything I changed here

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const container = document.getElementById('grid-container');
-
+const gridItems = document.getElementsByClassName('gridItem');
 // make grid
 function generateGrid() {
      for (var i=0; i < 257; i++) {
         var gridItem = document.createElement("div");
+gridItem.setAttribute('class', 'gridItem');
 gridItem.setAttribute('id', 'gridItem');
        container.appendChild(gridItem);
+       
      }
-}            
+} 
+
