1bdb95ab34571e17dfa67f6a523a142d72a337a2
Amelia Gustafson <amelia.gustafson200@gmail.com>
2024-03-11 10:49:48 -0500

    restart, change just about everything and add working mouseover event listener

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const container = document.getElementById('grid-container');
-const gridItems = document.getElementsByClassName('gridItem');
-// make grid
-function generateGrid() {
-     for (var i=0; i < 257; i++) {
-        var gridItem = document.createElement("div");
-gridItem.setAttribute('class', 'gridItem');
-gridItem.setAttribute('id', 'gridItem');
-       container.appendChild(gridItem);
-       
-     }
-} 
+const container = document.querySelector('.container');
+const createGrid = (amtOfGrids) => {
+    for ( i = 0; i < amtOfGrids; i++) {
+        const row = document.createElement('div')
+        row.setAttribute("class", "grid-row")
+        
+        for (j = 0; j < amtOfGrids; j++) {
+           const gridBox = document.createElement('div')
+           gridBox.setAttribute("class", "grid-box") 
+           gridBox.addEventListener('mouseenter', () =>{
+            gridBox.style.backgroundColor = 'black'
+           })
+           row.appendChild(gridBox)
+           
+        }
+        container.appendChild(row)
+    }
+}
+createGrid(16);
 
